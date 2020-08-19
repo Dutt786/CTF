1d10d7a85884393ba86f21ef45a5a15bfa47857d
CharuDatta <48205382+Dutt786@users.noreply.github.com>
2020-08-19 21:02:02 +0530

    Add files via upload

---
+++ macchanger.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+import subprocess
+subprocess.call("ifconfig eth0 down",shell=True)
+subprocess.call("ifconfig eth0 hw ether 00:11:22:33:44:55",shell=True)
+subprocess.call("ifconfig eth0 up",shell=True)
+exit()
+
